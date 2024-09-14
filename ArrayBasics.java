75965ad746acb6149df4836a0555af9419c2dde0
Md areeb Uddin <54110961+khan632@users.noreply.github.com>
2024-09-14 17:02:57 +0530

    Add files via upload
    Learn basics of array in java

---
+++ ArrayBasics.java
@@ -0,0 +1,47 @@
+package Array;
+
+import java.util.Scanner;
+public class Array {
+    public static void main (String [] args) {
+        // Array is collection of same data type value
+        // Array is a class in java and thatswhy it is non-primitive data type
+
+        // declaration
+        //int [] arr;
+
+        // initialisation
+        //int [] arr = new int [5];
+        //int [] arr1 = {3, -1, 5, 6, 8};
+
+        // display and input of array
+
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.netInt();
+//        for(int i = 0; i < n; i++) {
+//            arr[i] = sc.nextInt();
+//        }
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.println(arr[i]);
+//        }
+
+
+        // Linear Search
+
+//        int [] arr = {2, -1, 0, 5, 7, 8, 10, 11};
+//        int n = 8;
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            if(arr[i] == n) {
+//                System.out.println(i);
+//            }
+//        }
+//        System.out.println("Not Found");
+//        long start = System.currentTimeMillis();
+//        for (int i = 0; i < 100000; i++) {
+//
+//        }
+//
+//        long end = System.currentTimeMillis();
+//        System.out.println(end-start);
+    }
+}
